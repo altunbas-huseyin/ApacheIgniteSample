3366369e6653acd5bd9457a5b5c596f3dd92e123
huseyin-altunbas <huseyin.altunbas@tutkal.com.tr>
2018-12-03 00:18:17 +0300

    Init

---
+++ ApacheIgniteClient/ApacheIgniteClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache.Configuration;
+using Apache.Ignite.Core.Client;
+using Apache.Ignite.Core.Client.Cache;
+using System;
+
+namespace ApacheIgniteClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cfg = new IgniteClientConfiguration
+            {
+                Host = "127.0.0.1",
+                //UserName = "ignite",
+                //Password = "kg1mmcoXZU"
+            };
+
+            using (IIgniteClient ignite = Ignition.StartClient(cfg))
+            {
+
+                ICacheClient<int, string> cache = ignite.GetOrCreateCache<int, string>("cache1");
+                cache.Put(1, "Hello, World!");
+                string value = cache.Get(1);
+
+
+                //var cache = ignite.GetOrCreateCache<long, City>("SQL_PUBLIC_CITY");
+
+                //cache.Query(new SqlFieldsQuery("INSERT INTO City1(id, name) values (1, 'Forest Hill') "));
+            }
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+
+    public class City
+    {
+        [QuerySqlField(IsIndexed = true)]
+        public int id { get; set; }
+        [QuerySqlField]
+        public string name { get; set; }
+    }
+
+}
---
+++ ApacheIgniteServer/ApacheIgniteServer/Program.cs
@@ -0,0 +1,54 @@
+﻿using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache.Configuration;
+using Apache.Ignite.Core.Configuration;
+using System;
+
+namespace ApacheIgniteServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cfg = new IgniteConfiguration
+            {
+                DataStorageConfiguration = new DataStorageConfiguration
+                {
+                    DefaultDataRegionConfiguration = new DataRegionConfiguration
+                    {
+                        Name = "defaultRegion",
+                        PersistenceEnabled = true
+                    },
+                    DataRegionConfigurations = new[]
+                     {
+                            new DataRegionConfiguration
+                            {
+                                // Persistence is off by default.
+                                Name = "inMemoryRegion"
+                            }
+                        }
+                },
+                CacheConfiguration = new[]
+                 {
+                        new CacheConfiguration
+                        {
+                            // Default data region has persistence enabled.
+                            Name = "persistentCache"
+                        },
+                        new CacheConfiguration
+                        {
+                            Name = "inMemoryOnlyCache",
+                            DataRegionName = "inMemoryRegion"
+                        }
+                    }
+            };
+            cfg.WorkDirectory = @"C:\\workDirectory";
+            var t = Ignition.Start(cfg);
+            t.SetActive(true);
+            var tt = t.GetCluster().Ignite.GetCluster().GetNode().ConsistentId.ToString();
+            Console.WriteLine("Hello World! " + tt);
+
+            Console.ReadLine();
+        }
+    }
+}
